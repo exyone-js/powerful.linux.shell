eadf7034b9d3766456be1a2a49755af726c57bd3
exyone-js <exyone.dev@icloud.com>
2025-12-16 22:35:06 +0800

    Update autoremove.sh

--- ubuntu-server/autoremove.sh
+++ ubuntu-server/autoremove.sh
@@ -1,5 +1,9 @@
+# 移除不需要的包和依赖
 sudo apt autoremove --purge -y
+
+# 清理缓存
 sudo apt clean
 sudo apt autoclean
-# remove old kernels (keep the current and previous versions)
+
+# 删除旧内核（保留当前和上一个版本）
 sudo apt purge $(dpkg --list | grep 'linux-image' | awk '{print $2}' | grep -v $(uname -r) | grep -v $(uname -r | cut -d- -f1,2))
